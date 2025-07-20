fa3d1602a2a4bc524de5f53c19cf85ba4356b5ae
Mariam <mizzyj590@gmail.com>
2025-07-20 23:16:04 +0100

    Add python_introduction folder

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+sum_result = number1 + number2
+subtraction_result = number1 - number2
+product_result = number1 * number2
+
+print("Sum of", number1, "and", number2,"is", sum_result)
+print("Subtraction of", number1, "and", number2, "is", subtraction_result)
+print("Product of", number1, "and", number2, "is", product_result)
